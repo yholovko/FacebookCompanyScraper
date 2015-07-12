414f7fe8213ff5f7592cf15fb2d36017dfe33159
Jacob <golovko.yakov@gmail.com>
2015-07-12 23:29:07 +0300

    added write to file implementation

--- src/main/java/Excel.java
+++ src/main/java/Excel.java
@@ -4,10 +4,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -60,7 +57,57 @@ public class Excel {
         return facebookCompanies;
     }
 
-    public void writeToFile(File file, String text) {
+    public static void writeToFile(FacebookCompany facebookCompany, Result result) {
+        Workbook workbook = new XSSFWorkbook(); // Using XSSF for xlsx format, for xls use HSSF
+        Sheet resultSheet = workbook.createSheet("Sheet1");
 
+        int rowIndex = 0;
+        Row row = resultSheet.createRow(rowIndex++);
+
+        row.createCell(0).setCellValue("Name of the company");
+        row.createCell(1).setCellValue("Total Likes on Company FB Page");
+        row.createCell(2).setCellValue("Total Number of People who have been here");
+        row.createCell(3).setCellValue("Text of Post");
+        row.createCell(4).setCellValue("Date of Post");
+        row.createCell(5).setCellValue("Time of Post");
+        row.createCell(6).setCellValue("Who made the post (Company or someone else)");
+        row.createCell(7).setCellValue("Number of likes for the post");
+        row.createCell(8).setCellValue("Number of shares for the post");
+        row.createCell(9).setCellValue("Number of comments for the post");
+
+        for (int i = 0; i < result.getPosts().size(); i++) {
+            int cellIndex = 0;
+
+            row = resultSheet.createRow(++rowIndex);
+            row.createCell(cellIndex++).setCellValue(result.getNameOfTheCompany());
+            row.createCell(cellIndex++).setCellValue(result.getTotalLikesOnCompanyFBPage());
+            row.createCell(cellIndex++).setCellValue(result.getTotalNumberOfPeopleWhoHaveBeenHere());
+
+            Post post = result.getPosts().get(i);
+            row.createCell(cellIndex++).setCellValue(post.getTextOfPost());
+            row.createCell(cellIndex++).setCellValue(post.getDateOfPost());
+            row.createCell(cellIndex++).setCellValue(post.getTimeOfPost());
+            row.createCell(cellIndex++).setCellValue(post.getWhoMadeThePost());
+            row.createCell(cellIndex++).setCellValue(post.getNumberOfLikesForThePost());
+            row.createCell(cellIndex++).setCellValue(post.getNumberOfSharesForThePost());
+            row.createCell(cellIndex).setCellValue(post.getNumberOfCommentsForThePost());
+        }
+
+        try {
+            new File("result").mkdirs();
+
+            File file = new File("result/" + facebookCompany.getId() + ".xlsx");
+            if (file.exists()) {
+                file.delete();
+            }
+
+            FileOutputStream fos = new FileOutputStream(file);
+            workbook.write(fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -6,4 +6,36 @@ public class Result {
     private int totalLikesOnCompanyFBPage;
     private int totalNumberOfPeopleWhoHaveBeenHere;
     private List<Post> posts = new ArrayList<Post>();
+
+    public String getNameOfTheCompany() {
+        return nameOfTheCompany;
+    }
+
+    public void setNameOfTheCompany(String nameOfTheCompany) {
+        this.nameOfTheCompany = nameOfTheCompany;
+    }
+
+    public int getTotalLikesOnCompanyFBPage() {
+        return totalLikesOnCompanyFBPage;
+    }
+
+    public void setTotalLikesOnCompanyFBPage(int totalLikesOnCompanyFBPage) {
+        this.totalLikesOnCompanyFBPage = totalLikesOnCompanyFBPage;
+    }
+
+    public int getTotalNumberOfPeopleWhoHaveBeenHere() {
+        return totalNumberOfPeopleWhoHaveBeenHere;
+    }
+
+    public void setTotalNumberOfPeopleWhoHaveBeenHere(int totalNumberOfPeopleWhoHaveBeenHere) {
+        this.totalNumberOfPeopleWhoHaveBeenHere = totalNumberOfPeopleWhoHaveBeenHere;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void addPost(Post post) {
+        this.posts.add(post);
+    }
 }
