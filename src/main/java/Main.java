635f68e75436ca3cc72f205218185f9a395d70fb
Jacob <golovko.yakov@gmail.com>
2015-07-13 02:29:14 +0300

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,22 @@ public class Main {
     public static void main(String[] args) {
         List<FacebookCompany> facebookCompanies = Excel.getInformation("C:\\Users\\Jacob\\Documents\\Projects\\FacebookCompanyScraper\\FB Page Links All.xlsx");
 
+
+        for (FacebookCompany facebookCompany : facebookCompanies) {
+            Scraper scraperThread = new Scraper(facebookCompany);
+            scraperThread.run();
+//            scraperThread.setName(facebookCompany.getName());
+//            scraperThread.start();
+//
+//            synchronized (scraperThread) {
+//                try {
+//                    System.out.println("Waiting for "+ scraperThread.getName() +" to complete...");
+//                    scraperThread.wait();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println(scraperThread.getName() + "Finished");
+//            }
+        }
     }
 }
--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,3 +1,48 @@
-public class Scraper {
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+public class Scraper /*extends Thread*/ {
+    private FacebookCompany facebookCompany;
+    private WebDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME);
+    private Result result = new Result();
+
+    public Scraper(FacebookCompany facebookCompany) {
+        this.facebookCompany = facebookCompany;
+    }
+
+    public WebDriver connectTo(String url) {
+        while (true) {
+            try {
+                ((HtmlUnitDriver) driver).setJavascriptEnabled(true);
+
+                driver.get(url);
+                break;
+            } catch (Exception e) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+        return driver;
+    }
+
+    //@Override
+    public void run() {
+//        synchronized (this) {
+        WebDriver driver = connectTo(facebookCompany.getLink());
+
+        Document doc = Jsoup.parse(driver.getPageSource());
+        doc.select("#js_b > div > ul > li:nth-child > a");
+
+        result.setNameOfTheCompany(facebookCompany.getName());
+
+        //notify();
+//        }
+    }
 }
