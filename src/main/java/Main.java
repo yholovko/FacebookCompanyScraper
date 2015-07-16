7b1ccb2f9aa807adba63470bf633e3efdf934936
Jacob <golovko.yakov@gmail.com>
2015-07-16 19:25:14 +0300

    added access token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,20 @@
-import facebook4j.FacebookException;
-
 import java.util.List;
-
 /**
+ * Access token for 1 hour
  * Get an access token from:
  * https://developers.facebook.com/tools/explorer
  * Copy and paste 'accessTokenString' value
+ *
+ * OR (STRONGLY RECOMMENDED)
+ * https://smashballoon.com/custom-facebook-feed/access-token/
  */
 
 public class Main {
-    final public static String accessTokenString = "CAACEdEose0cBAKPzzkJMcdOe4j2qgZB4mLOl1FXlZArMvCtGTAoz5w9aG1D57Ex4T4HOtLlvu8KoUFWNWpKk3ZCSKShGD9ZBcBvEFMsCkOQv9NvwHcNubHQfJGr9C2hdEJradPIK8STssUSZAHUB0rgcByKYf7vPcYlZCYYJ7EhZCY4M0epmxYeJMVhigABCM5mm3rari2JEOpKjxB2oqWdDi0prdPVIEAZD";
+    final public static String accessTokenString = "763885633733453|UPL0nXC91GOdVIomVTYAlOB2JUs";
 
-    public static void main(String[] args) throws FacebookException {
+    public static void main(String[] args) {
         List<FacebookCompany> facebookCompanies = Excel.getInformation("/home/jacob/Documents/FacebookCompanyScraper/FB Page Links All.xlsx");
+
         for (FacebookCompany facebookCompany : facebookCompanies) {
             System.out.println(String.format("Start to processing %s", facebookCompany.getLink()));
             Scraper scraper = new Scraper(facebookCompany);
