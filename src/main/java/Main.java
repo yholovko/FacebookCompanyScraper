1aafc0a9cfb286713cbe31187f57892436958169
Jacob <golovko.yakov@gmail.com>
2015-07-16 18:30:52 +0300

    optimize requests to facebook

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,26 @@
+import facebook4j.FacebookException;
+
 import java.util.List;
 
 /**
  * Get an access token from:
  * https://developers.facebook.com/tools/explorer
- * Copy and paste it in Scraper.java ==> 'accessTokenString' value
+ * Copy and paste 'accessTokenString' value
  */
 
 public class Main {
-    public static void main(String[] args) {
+    final public static String accessTokenString = "CAACEdEose0cBAKPzzkJMcdOe4j2qgZB4mLOl1FXlZArMvCtGTAoz5w9aG1D57Ex4T4HOtLlvu8KoUFWNWpKk3ZCSKShGD9ZBcBvEFMsCkOQv9NvwHcNubHQfJGr9C2hdEJradPIK8STssUSZAHUB0rgcByKYf7vPcYlZCYYJ7EhZCY4M0epmxYeJMVhigABCM5mm3rari2JEOpKjxB2oqWdDi0prdPVIEAZD";
+
+    public static void main(String[] args) throws FacebookException {
         List<FacebookCompany> facebookCompanies = Excel.getInformation("/home/jacob/Documents/FacebookCompanyScraper/FB Page Links All.xlsx");
         for (FacebookCompany facebookCompany : facebookCompanies) {
+            System.out.println(String.format("Start to processing %s", facebookCompany.getLink()));
             Scraper scraper = new Scraper(facebookCompany);
-            Excel.writeToFile(facebookCompany, scraper.getInformation());
+            Result result = scraper.getInformation();
+            if (result != null) {
+                Excel.writeToFile(facebookCompany, result);
+                System.out.println(String.format("Finished %s", facebookCompany.getLink()));
+            }
         }
     }
 }
--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,82 +1,78 @@
 import facebook4j.*;
-import facebook4j.Post;
 import facebook4j.auth.AccessToken;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Scraper {
-    final private String accessTokenString = "CAACEdEose0cBAKNX4VYeZBgnu3u5QrDbh6TZAdHBhxum2kami0nYhWGWjWMcMuNsAFQpm1v72wZCnEesL19UczqZAzaLTTWjNU2UjA8eUElIxOZCgJ0ToPKT0MZAf4HeeyEHMExXT2mb9jV79mcX5UrB4gL3rxldaGzNMfizZCU86l4PfycEU6uXcR4IAOdUN0seuYqJyPS088UHGhVL4mIJG3OEM8XKO8ZD";
     private FacebookCompany facebookCompany;
-    private Result result = new Result();
     private Facebook facebook = new FacebookFactory().getInstance();
 
     public Scraper(FacebookCompany facebookCompany) {
         this.facebookCompany = facebookCompany;
 
         facebook.setOAuthAppId("", "");
-        facebook.setOAuthAccessToken(new AccessToken(accessTokenString));
+        facebook.setOAuthAccessToken(new AccessToken(Main.accessTokenString));
     }
 
-    private int getLikes(String companyName){
-        try {
-            return facebook.getPage(companyName).getLikes();
-        } catch (FacebookException e) {
-            e.printStackTrace();
+    private int getLikes(Page facebookPage) {
+        Integer likes = facebookPage.getLikes();
+        if (likes != null) {
+            return likes;
         }
         return 0;
     }
 
-    private int getTotalNumberOfPeople(String companyName){
-        try {
-            return facebook.getPage(companyName).getTalkingAboutCount();
-        } catch (FacebookException e) {
-            e.printStackTrace();
+    private int getTotalNumberOfPeople(Page facebookPage) {
+        Integer totalNumber = facebookPage.getTalkingAboutCount();
+        if (totalNumber != null) {
+            return totalNumber;
         }
         return 0;
     }
 
     private int getLikesForPost(Post post){
-        int count = 0;
-        PagableList<Like> fetchedLikes = null;
+        Integer totalLikes;
         try {
-            fetchedLikes = post.getLikes();
-            Paging<Like> paging;
-            do {
-                count += fetchedLikes.size();
-                paging = fetchedLikes.getPaging();
-            } while ((paging != null) && ((fetchedLikes = facebook.fetchNext(paging)) != null));
+            Summary summary = facebook.getPostLikes(post.getId(), new Reading().limit(0).summary()).getSummary();
+            if (summary != null) {
+                totalLikes = summary.getTotalCount();
+                if (totalLikes != null) {
+                    return totalLikes;
+                }
+            }
         } catch (FacebookException e) {
             e.printStackTrace();
         }
-        return count;
+        return 0;
     }
 
     private int getCommentsForPost(Post post){
-        int count = 0;
-        PagableList<Comment> fetchedComments = null;
+        Integer totalComments;
         try {
-            fetchedComments = post.getComments();
-            Paging<Comment> paging;
-            do {
-                count += fetchedComments.size();
-                paging = fetchedComments.getPaging();
-            } while ((paging != null) && ((fetchedComments = facebook.fetchNext(paging)) != null));
+            totalComments = facebook.getPostComments(post.getId(), new Reading().limit(0).summary()).getSummary().getTotalCount();
+            if (totalComments != null) {
+                return totalComments;
+            }
         } catch (FacebookException e) {
             e.printStackTrace();
         }
-        return count;
+        return 0;
     }
 
     private List<Post> getPosts(String companyName){
         List<Post> posts = new ArrayList<Post>();
         try {
-            ResponseList<facebook4j.Post> fetchedPosts = facebook.getPosts(facebook.getPage(companyName).getId(), new Reading().limit(100));
-            Paging<Post> paging;
-            do {
-                posts.addAll(fetchedPosts);
-                paging = fetchedPosts.getPaging();
-            } while ((paging != null) && ((fetchedPosts = facebook.fetchNext(paging)) != null));
+            ResponseList<facebook4j.Post> fetchedPosts = facebook.getPosts(facebook.getPage(companyName).getId(), new Reading().fields("id", "message", "link", "shares", "from").limit(100));
+            if (fetchedPosts != null) {
+                Paging<Post> paging;
+                do {
+                    posts.addAll(fetchedPosts);
+                    paging = fetchedPosts.getPaging();
+                    System.out.println(String.format("Got %s posts from %s", posts.size(), facebookCompany.getLink()));
+                } while ((paging != null) && ((fetchedPosts = facebook.fetchNext(paging)) != null));
+            }
         } catch (FacebookException e) {
             e.printStackTrace();
         }
@@ -84,25 +80,46 @@ public class Scraper {
         return posts;
     }
 
-    public Result getInformation(){
-        final String companyName = facebookCompany.getLink().replaceAll("https://www.facebook.com/","");
+    public Result getInformation() {
+        final String companyNameFromUrl = facebookCompany.getLink().replaceAll("https://www.facebook.com/", "");
+        Result result = new Result();
 
-        result.setNameOfTheCompany(facebookCompany.getName());
-        result.setTotalLikesOnCompanyFBPage(getLikes(companyName));
-        result.setTotalNumberOfPeopleWhoHaveBeenHere(getTotalNumberOfPeople(companyName));
+        try {
+            Page facebookPage = facebook.getPage(companyNameFromUrl, new Reading().fields("likes", "talkingAboutCount"));
 
-        FacebookPost facebookPost = new FacebookPost();
+            final String companyNameOnPage = facebookPage.getName();
+            result.setNameOfTheCompany(facebookCompany.getName());
+            result.setTotalLikesOnCompanyFBPage(getLikes(facebookPage));
+            result.setTotalNumberOfPeopleWhoHaveBeenHere(getTotalNumberOfPeople(facebookPage));
 
-        for (Post post : getPosts(companyName)) {
-            facebookPost.setNumberOfLikesForThePost(getLikesForPost(post));
-            facebookPost.setNumberOfCommentsForThePost(getCommentsForPost(post));
-            facebookPost.setNumberOfSharesForThePost(post.getSharesCount());
-            facebookPost.setDateAndTimeOfPost(post.getCreatedTime());
-            facebookPost.setWhoMadeThePost(post.getFrom().getName());
-            facebookPost.setTextOfPost(post.getMessage() + " " + post.getLink());
+            int count = 0;
+            List<Post> posts = getPosts(companyNameFromUrl);
 
-            result.addPosts(facebookPost);
+            for (Post post : posts) {
+                Integer sharedCount = post.getSharesCount();
+                String postAuthor = post.getFrom().getName();
+                String message = post.getMessage();
+                URL link = post.getLink();
+
+                FacebookPost facebookPost = new FacebookPost();
+                facebookPost.setNumberOfLikesForThePost(getLikesForPost(post));
+                facebookPost.setNumberOfCommentsForThePost(getCommentsForPost(post));
+                facebookPost.setNumberOfSharesForThePost((sharedCount != null) ? sharedCount : 0);
+                facebookPost.setDateAndTimeOfPost(post.getCreatedTime());
+                facebookPost.setWhoMadeThePost((companyNameOnPage.equals(postAuthor)) ? facebookCompany.getName() : postAuthor);
+                facebookPost.setTextOfPost(((message != null) ? message : "") + " " + ((link != null) ? link.toString() : ""));
+
+                result.addPosts(facebookPost);
+
+                if (count % 25 == 0) System.out.println(count + " / " + posts.size());
+                count++;
+            }
+        } catch (FacebookException e) {
+            System.err.println(String.format("Page %s not found or other error. See the stack trace", facebookCompany.getLink()));
+            e.printStackTrace();
+            result = null;
         }
+
         return result;
     }
 }
